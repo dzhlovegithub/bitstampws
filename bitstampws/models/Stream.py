5c469257295b519987c0de66646d21f8481d5b4f
oxsoftdev <oxsoftdev@gmail.com>
2017-08-01 20:02:38 -0400

    add(model): Stream

---
+++ bitstampws/models/Stream.py
@@ -0,0 +1,19 @@
+from ._BaseModel import BaseModel
+
+
+class Stream(BaseModel):
+
+    def __init__(self, timestamp, datetime, payload):
+        self.event = payload['event']
+        self.channel = payload['channel']
+        if 'data' in payload:
+            book = self.channel.split('_')[-1]
+            data = json.loads(payload['data'])
+            if self.event.startswith('live_trades'):
+                logger.info(data)
+            elif self.event.startswith('order_book'):
+                pass
+            elif self.event.startswith('diff_order_book'):
+                pass
+            elif self.event.startswith('live_orders'):
+                pass
