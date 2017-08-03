141f71f8ad4abff98035e323da462c1a0075305a
oxsoftdev <oxsoftdev@gmail.com>
2017-08-03 01:07:03 -0400

    fix(Stream)

--- bitstampws/models/Stream.py
+++ bitstampws/models/Stream.py
@@ -6,17 +6,28 @@ from ._BaseModel import BaseModel
 class Stream(BaseModel):
 
     def __init__(self, timestamp, datetime, payload):
+        self._timestamp = timestamp
+        self._datetime = datetime
         self.event = payload['event']
         self.channel = payload['channel']
         if 'data' in payload:
-            book = self.channel.split('_')[-1]
             data = json.loads(payload['data'])
-            if self.event.startswith('live_trades'):
-                logger.info(data)
-            elif self.event.startswith('order_book'):
-                pass
-            elif self.event.startswith('diff_order_book'):
-                pass
-            elif self.event.startswith('live_orders'):
-                pass
+            if self.channel.startswith('live_trades'):
+                self.book = self.get_book(2)
+            elif self.channel.startswith('order_book'):
+                self.book = self.get_book(2)
+            elif self.channel.startswith('diff_order_book'):
+                self.book = self.get_book(3)
+            elif self.channel.startswith('live_orders'):
+                self.book = self.get_book(2)
 
+    def get_book(self, length):
+        if len(self.channel.split('_')) == (length + 1):
+            return self.channel.split('_')[-1]
+        elif len(self.channel.split('_')) == length:
+            return 'btcusd'
+
+    def __repr__(self):
+        return "Stream({Stream})".format(
+            Stream=self._repr('event', 'channel', 'book')
+        )
