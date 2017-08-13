b52062bd40b82253122bcb289103567e89905c97
oxsoftdev <oxsoftdev@gmail.com>
2017-08-13 08:48:11 -0700

    update(model): DiffOrder plus some fixes

--- bitstampws/models/DiffOrderBook.py
+++ bitstampws/models/DiffOrderBook.py
@@ -1,3 +1,40 @@
-class DiffOrderBook:
-    pass
+from decimal import Decimal
+
+from ._BaseModel import BaseModel
+
+
+class _DiffOrder:
+
+    def __init__(self), amount, price):
+        self.amount = Decimal(str(amount))
+        self.price = Decimal(str(price))
+
+
+class DiffOrderBook(BaseModel):
+
+    def __init__(self, timestamp, datetime, book, data):
+        self._timestamp = timestamp
+        self._datetime = datetime
+        self.book = book
+
+        for (param, value) in data.items():
+            if param == 'timestamp':
+                self.timestamp = int(str(timestamp))
+            elif param == 'bids':
+                self.bids = self._build_difforders_list(value)
+            elif param == 'asks':
+                self.asks = self._build_difforders_list(value)
+
+    def _build_difforders_list(self, data):
+        _orders = []
+        for order in data:
+            _orders.append(_DiffOrder(order[1], order[0]))
+        return _orders
+
+    def __repr__(self):
+        return "DiffOrderBook(book={book}, {num_bids} bids, {num_asks} asks)".format(
+            book = self.book,
+            num_bids = len(self.bids),
+            num_asks = len(self.asks)
+        )
 
--- bitstampws/models/Stream.py
+++ bitstampws/models/Stream.py
@@ -1,6 +1,7 @@
 import json
 
 from ._BaseModel import BaseModel
+from .DiffOrder import DiffOrder
 from .Order import Order
 from .OrderBook import OrderBook
 from .Trade import Trade
@@ -16,24 +17,25 @@ class Stream(BaseModel):
 
         data = json.loads(payload['data'])
         if self.channel.startswith('live_trades'):
-            self.book = self.get_book(2)
-            self.data = self.build_data_object(data, Trade)
+            self.book = self._get_book(2)
+            self.data = self._build_data_object(data, Trade)
         elif self.channel.startswith('order_book'):
-            self.book = self.get_book(2)
-            self.data = self.build_data_object(data, OrderBook)
+            self.book = self._get_book(2)
+            self.data = self._build_data_object(data, OrderBook)
         elif self.channel.startswith('diff_order_book'):
-            self.book = self.get_book(3)
+            self.book = self._get_book(3)
+            self.data = self._build_data_object(data, DiffOrder)
         elif self.channel.startswith('live_orders'):
-            self.book = self.get_book(2)
-            self.data = self.build_data_object(data, Order)
+            self.book = self._get_book(2)
+            self.data = self._build_data_object(data, Order)
 
-    def get_book(self, length):
+    def _get_book(self, length):
         if len(self.channel.split('_')) == (length + 1):
             return self.channel.split('_')[-1]
         elif len(self.channel.split('_')) == length:
             return 'btcusd'
 
-    def build_data_object(self, data, type):
+    def _build_data_object(self, data, type):
         return type(self._timestamp, self._datetime, self.book, data)
 
     def __repr__(self):
